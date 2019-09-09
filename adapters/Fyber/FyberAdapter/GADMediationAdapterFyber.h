325f52b7bb08ff544ad45344b7912529d898ea4b
avi gelkop <avi.gelkop@fyber.com>
2019-09-09 12:25:44 +0300

    continue the old API

--- adapters/Fyber/FyberAdapter/GADMediationAdapterFyber.h
+++ adapters/Fyber/FyberAdapter/GADMediationAdapterFyber.h
@@ -12,7 +12,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface GADMediationAdapterFyber : NSObject <GADMAdNetworkAdapter>
+@interface GADMediationAdapterFyber : NSObject
 
 @end
 
--- adapters/Fyber/FyberAdapter/GADMediationAdapterFyber.m
+++ adapters/Fyber/FyberAdapter/GADMediationAdapterFyber.m
@@ -10,21 +10,31 @@
 #import "GADMediationAdapterFyber.h"
 
 @import IASDKCore;
+@import IASDKVideo;
+@import IASDKMRAID;
+@import CoreLocation;
 
 static NSString * const _Nonnull kFYBApplicationID = @"applicationId";
 static NSString * const _Nonnull kFYBSpotID = @"spotId";
 
-static NSString * const _Nonnull kFYBBanner = @"kFYBBanner";
-static NSString * const _Nonnull kFYBInterstitial = @"kFYBInterstitial";
-static NSString * const _Nonnull kFYBRewarded = @"kFYBRewarded";
+typedef NS_ENUM(NSInteger, FYBAdType) {
+  FYBAdTypeBanner = 0,
+  FYBAdTypeInterstitial = 1,
+  FYBAdTypeRewarded = 2,
+};
 
-@interface GADMediationAdapterFyber ()
+@interface GADMediationAdapterFyber () <IAUnitDelegate, GADMediationAdapter, GADMAdNetworkAdapter>
 
 @property (nonatomic, nonnull) id<GADMAdNetworkConnector> connector;
 
-@property (nonatomic, nullable, strong) NSMutableDictionary <NSString *, IAUnitController *> *IAUnitControllers;
-@property (nonatomic, nullable, strong) NSMutableDictionary <NSString *, IAContentController *> *IAContentControllers;
-@property (nonatomic, nullable, strong) NSMutableDictionary <NSString *, IAAdSpot *> *IAAdspots;
+@property (nonatomic, strong, nonnull) IAViewUnitController *viewUnitController;
+@property (nonatomic, strong, nonnull) IAFullscreenUnitController *fullscreenUnitController;
+
+@property (nonatomic, strong, nonnull) IAMRAIDContentController *MRAIDContentController;
+@property (nonatomic, strong, nonnull) IAVideoContentController *videoContentController;
+
+@property (nonatomic, strong) IAAdSpot *adSpot;
+@property (nonatomic, weak) IAAdView *adView;
 
 @end
 
@@ -38,14 +48,23 @@ static NSString * const _Nonnull kFYBRewarded = @"kFYBRewarded";
     return self.class;
 }
 
++ (GADVersionNumber)adSDKVersion {
+    GADVersionNumber version = {0};
+    return version;
+}
+
+
++ (GADVersionNumber)version {
+    GADVersionNumber version = {0};
+    return version;
+}
+
+
 - (instancetype)initWithGADMAdNetworkConnector:(id<GADMAdNetworkConnector>)connector {
     self = [super init];
     
     if (self) {
         _connector = connector;
-        _IAUnitControllers = [NSMutableDictionary new];
-        _IAContentControllers = [NSMutableDictionary new];
-        _IAAdspots = [NSMutableDictionary new];
     }
     
     if (self.connector) {
@@ -62,16 +81,35 @@ static NSString * const _Nonnull kFYBRewarded = @"kFYBRewarded";
 }
 
 - (void)getBannerWithSize:(GADAdSize)adSize {
+    __weak typeof(self) weakSelf = self;
     NSString *spotId = [self.connector credentials][kFYBSpotID];
-    if (self.IAAdspots[spotId]) {
-        
-    } else {
-        [self initSpot:spotId adType:kFYBBanner];
-    }
+    
+    [self initSpot:spotId adType:FYBAdTypeBanner];
+    [self.adSpot fetchAdWithCompletion:^(IAAdSpot * _Nullable adSpot, IAAdModel * _Nullable adModel, NSError * _Nullable error) {
+        if (error) {
+            [weakSelf.connector adapter:weakSelf didFailAd:error];
+        } else {
+            [weakSelf.connector adapter:weakSelf didReceiveAdView:weakSelf.viewUnitController.adView];
+        }
+    }];
 }
 
 - (void)getInterstitial {
+    __weak typeof(self) weakSelf = self;
+    NSString *spotId = [self.connector credentials][kFYBSpotID];
+    
+    [self initSpot:spotId adType:FYBAdTypeInterstitial];
+    [self.adSpot fetchAdWithCompletion:^(IAAdSpot * _Nullable adSpot, IAAdModel * _Nullable adModel, NSError * _Nullable error) {
+        if (error) {
+            [weakSelf.connector adapter:weakSelf didFailAd:error];
+        } else {
+            [weakSelf.connector adapterDidReceiveInterstitial:weakSelf];
+        }
+    }];
+}
 
+- (void)loadRewardedAdForAdConfiguration:(GADMediationRewardedAdConfiguration *)adConfiguration completionHandler:(GADMediationRewardedLoadCompletionHandler)completionHandler {
+    
 }
 
 - (void)stopBeingDelegate {
@@ -79,13 +117,113 @@ static NSString * const _Nonnull kFYBRewarded = @"kFYBRewarded";
 }
 
 - (void)presentInterstitialFromRootViewController:(UIViewController *)rootViewController {
-    
+    [self.fullscreenUnitController showAdAnimated:YES completion:nil];
 }
 
 #pragma mark - Service
 
-- (void)initSpot:(NSString * _Nonnull)spotId adType:(NSString * _Nonnull)adType {
+- (void)initSpot:(NSString * _Nonnull)spotId adType:(FYBAdType)adType {
+    __weak typeof(self) weakSelf = self;
+    
+    IAAdRequest *request = [IAAdRequest build:^(id<IAAdRequestBuilder>  _Nonnull builder) {
+        builder.useSecureConnections = NO;
+        builder.spotID = spotId;
+        builder.timeout = 10;
+        builder.keywords = [[self.connector.userKeywords valueForKey:@"description"] componentsJoinedByString:@""];
+        
+        if ([self.connector userHasLocation]) {
+            builder.location = [[CLLocation alloc] initWithLatitude:self.connector.userLatitude longitude:weakSelf.connector.userLongitude];
+        }
+    }];
+    
+    if (adType == FYBAdTypeBanner) {
+        _MRAIDContentController = [IAMRAIDContentController build:^(id<IAMRAIDContentControllerBuilder>  _Nonnull builder) {}];
+        
+        _viewUnitController = [IAViewUnitController build:^(id<IAViewUnitControllerBuilder>  _Nonnull builder) {
+            builder.unitDelegate = self;
+            [builder addSupportedContentController:self.MRAIDContentController];
+        }];
+        
+        _adSpot = [IAAdSpot build:^(id<IAAdSpotBuilder>  _Nonnull builder) {
+            builder.adRequest = request;
+            [builder addSupportedUnitController:self.viewUnitController];
+        }];
+        
+    } else if (adType == FYBAdTypeInterstitial) {
+        _MRAIDContentController = [IAMRAIDContentController build:^(id<IAMRAIDContentControllerBuilder>  _Nonnull builder) {}];
+        _videoContentController = [IAVideoContentController build:^(id<IAVideoContentControllerBuilder>  _Nonnull builder) {}];
+        
+        _viewUnitController = [IAViewUnitController build:^(id<IAViewUnitControllerBuilder>  _Nonnull builder) {
+            builder.unitDelegate = self;
+            [builder addSupportedContentController:self.videoContentController];
+            [builder addSupportedContentController:self.MRAIDContentController];
+        }];
+        
+        _fullscreenUnitController = [IAFullscreenUnitController build:^(id<IAFullscreenUnitControllerBuilder>  _Nonnull builder) {
+            builder.unitDelegate = self;
+            [builder addSupportedContentController:self.videoContentController];
+            [builder addSupportedContentController:self.MRAIDContentController];
+        }];
+        
+        _adSpot = [IAAdSpot build:^(id<IAAdSpotBuilder>  _Nonnull builder) {
+            builder.adRequest = request;
+            [builder addSupportedUnitController:self.viewUnitController];
+            [builder addSupportedUnitController:self.fullscreenUnitController];
+        }];
+        
+    } else if (adType == FYBAdTypeRewarded) {
+        _videoContentController = [IAVideoContentController build:^(id<IAVideoContentControllerBuilder>  _Nonnull builder) {}];
+        
+        _fullscreenUnitController = [IAFullscreenUnitController build:^(id<IAFullscreenUnitControllerBuilder>  _Nonnull builder) {
+            builder.unitDelegate = self;
+            [builder addSupportedContentController:self.videoContentController];
+        }];
+        
+        _adSpot = [IAAdSpot build:^(id<IAAdSpotBuilder>  _Nonnull builder) {
+            builder.adRequest = request;
+            [builder addSupportedUnitController:self.fullscreenUnitController];
+        }];
+    } else {
+        NSLog(@"<fyber> error");
+    }
+}
 
+#pragma mark - IAUnitDelegate
+
+- (UIViewController * _Nonnull)IAParentViewControllerForUnitController:(IAUnitController * _Nullable)unitController {
+    return [self.connector viewControllerForPresentingModalView];
+}
+
+- (void)IAAdDidReceiveClick:(IAUnitController * _Nullable)unitController {
+    [self.connector adapterDidGetAdClick:self];
+}
+
+- (void)IAUnitControllerWillPresentFullscreen:(IAUnitController * _Nullable)unitController {
+    if (unitController == self.viewUnitController) {
+        [self.connector adapterWillPresentFullScreenModal:self];
+    } else if (unitController == self.fullscreenUnitController) {
+        [self.connector adapterWillPresentInterstitial:self];
+    }
+}
+
+- (void)IAUnitControllerWillDismissFullscreen:(IAUnitController * _Nullable)unitController {
+    if (unitController == self.viewUnitController) {
+        [self.connector adapterWillDismissFullScreenModal:self];
+    } else if (unitController == self.fullscreenUnitController) {
+        [self.connector adapterWillDismissInterstitial:self];
+    }
+}
+
+- (void)IAUnitControllerDidDismissFullscreen:(IAUnitController * _Nullable)unitController {
+    if (unitController == self.viewUnitController) {
+        [self.connector adapterDidDismissFullScreenModal:self];
+    } else if (unitController == self.fullscreenUnitController) {
+        [self.connector adapterDidDismissInterstitial:self];
+    }
+}
+
+- (void)IAUnitControllerWillOpenExternalApp:(IAUnitController * _Nullable)unitController {
+    [self.connector adapterWillLeaveApplication:self];
 }
 
 @end
